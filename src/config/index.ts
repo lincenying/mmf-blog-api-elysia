d9043c2ecfdebb6f14f0e789ce9ba49558fdef99
LinCenYing <lincenying@qq.com>
2026-03-18 23:59:02 +0800

    refactor(config): 移除不再使用的配置项
    移除 `shihua.js` 中的 `API_KEY`, `APP_ID`, `SECRET_KEY` 以及相应的导入语句
    移除 `tujidao.js` 中的 `cookies` 导出以及相应的导入语句

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,10 @@
 import * as _qiniu from './qiniu.js'
-import { API_KEY, APP_ID, SECRET_KEY } from './shihua.js'
 
 export { secretClient, secretServer } from './secret.js'
-export { cookies as tujidao } from './tujidao.js'
 
 // MD5 加密前缀, 如用户的密码是 123456, 存到数据库将会变成 md5('!@#$%(*&^)' + '123456')
 export const md5Pre = '!@#$%(*&^)'
 
-export const shihua = {
-    APP_ID,
-    API_KEY,
-    SECRET_KEY,
-}
 export const qiniu = _qiniu
 // API域名
 export const domain = 'https://api.mmxiaowu.com/'
