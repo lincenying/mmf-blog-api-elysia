c2167ca0065e718bced5491e787d3740523ecd36
LinCenYing <lincenying@qq.com>
2026-03-20 10:07:52 +0800

    fix(src): 修复路由处理和文件返回逻辑
    移除了不再使用的 `responseWrapperMiddleware` 和 `ApiError` 导入。
    将 `/sm/*` 路由的返回值从空字符串改为直接返回空字符串。
    将 `/*` 路由的错误信息从抛出 `ApiError` 改为返回字符串 'Page Not Found'。
    添加了 `/favicon.ico` 和 `/robots.txt` 路由，分别返回相应的文件。

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 import { mkdir } from 'node:fs/promises'
-import { Elysia } from 'elysia'
+import { Elysia, file } from 'elysia'
 
 import { config } from '@/config'
 import { logger } from '@/utils/logger'
 import { accessLoggerMiddleware } from './middleware/access-logger'
-import { responseWrapperMiddleware } from './middleware/response-wrapper'
 import { createStaticConfig, createSwaggerConfig } from './plugins'
 import { adminRouter } from './routes/admin'
 import { backendRouter } from './routes/backend'
 import { frontendRouter } from './routes/frontend'
 import { uploadRouter } from './routes/upload'
-import { ApiError } from './types'
 
 (async () => {
     const UPLOAD_DIR = './uploads'
@@ -25,17 +23,14 @@ const app = new Elysia({
     .use(createStaticConfig())
     .use(createSwaggerConfig())
     .use(accessLoggerMiddleware)
-    .use(responseWrapperMiddleware)
     .use(frontendRouter)
     .use(backendRouter)
     .use(uploadRouter)
     .use(adminRouter)
-    .all('/sm/*', () => {
-        return ''
-    })
-    .all('/*', () => {
-        throw new ApiError(404, 'Not Found')
-    })
+    .get('/favicon.ico', file('./public/favicon.ico'))
+    .get('/robots.txt', file('./public/robots.txt'))
+    .all('/sm/*', () => '')
+    .all('/*', () => 'Page Not Found')
     .listen(config.server.port)
 
 // 获取正确的访问信息
